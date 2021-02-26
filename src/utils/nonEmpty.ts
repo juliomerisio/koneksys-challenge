a75ba395cd33b1f004118d1ba5371f3d5d29d8e0
juliomerisio <contatomerisio@gmail.com>
2021-02-26 17:41:59 -0300

    fix: not allow not specified sheets

--- src/utils/nonEmpty.ts
+++ src/utils/nonEmpty.ts
@@ -1,11 +1,30 @@
 const IGNORED_ROWS = 2
 
+const REQUIRED_COLUMNS = [
+  '#',
+  'Age',
+  'Base Salary',
+  'College',
+  'Experience',
+  'Height',
+  'Player Name',
+  'Pos',
+  'Status',
+  'Weight',
+]
+
 export function nonEmpty(elements: { [key: string]: unknown[] }, line: number) {
   const errors: string[] = []
 
   Object.entries(elements).forEach(([name, value]) => {
-    if (!value.length) {
-      errors.push(`${name} on line ${line + IGNORED_ROWS} can't be null.`)
+    if (REQUIRED_COLUMNS.includes(name)) {
+      if (!value.length) {
+        errors.push(`${name} on line ${line + IGNORED_ROWS} can't be null.`)
+      }
+    }
+
+    if (!REQUIRED_COLUMNS.includes(name)) {
+      errors.push(`${name} is not allowed`)
     }
   })
 
