705905a7c20bf631662ea6aba5b0b0b4bc4e857d
juliomerisio <contatomerisio@gmail.com>
2021-02-25 23:00:05 -0300

    fix: small fix on row calculation

--- src/utils/nonEmpty.ts
+++ src/utils/nonEmpty.ts
@@ -1,9 +1,11 @@
+const IGNORED_ROWS = 2
+
 export function nonEmpty(elements: { [key: string]: unknown[] }, line: number) {
   const errors: string[] = []
 
   Object.entries(elements).forEach(([name, value]) => {
     if (!value.length) {
-      errors.push(`${name} on line ${line} can't be null.`)
+      errors.push(`${name} on line ${line + IGNORED_ROWS} can't be null.`)
     }
   })
 
